a343e4d0402eb36ab778ab30ff78f1dce0166907
Utsav Mangal <mangalutsav1@gmail.com>
2016-10-07 00:38:50 +0530

    updated app

--- app.js
+++ app.js
@@ -11,20 +11,34 @@ http.listen (app.get('port'),function() {
   console.log("listening to port number "+app.get('port'));
 });
 io.sockets.on('connection', function (socket) {
+  var users = [];
   socket.on('join', function (data) {
      id=data;
+     users.push(id);
+     users[id] = [];
+     console.log(id);
     socket.join(id); // We are using room of socket io
       io.sockets.in(id).emit('got email',id);
     });
     socket.on('send data',function(data){
       socket.item=data;
-      Itemdata.push(data);
-      console.log(Itemdata);
-      io.sockets.in(id).emit('new data',Itemdata);
+      data = data.split(" ");
+      person = data[0];
+      data.splice(0,1);
+      message = data.toString();
+      console.log(message);
+      console.log(person);
+      users[person].push(message);
+      console.log(users[person]);
+      io.sockets.in(id).emit('new data',users[person]);
       });
       socket.on('delete',function(data){
-        Itemdata.splice(Itemdata.indexOf(data),1);
-        io.sockets.in(id).emit('deleted',Itemdata);
+        data = data.split(" ");
+        person = data[0];
+        data.splice(0,1);
+        message = data.toString();
+        Itemdata.splice(users[person].indexOf(message),1);
+        io.sockets.in(id).emit('deleted',users[person]);
       });
 
   });
