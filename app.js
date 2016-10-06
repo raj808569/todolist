1ee6f5f2aa8f0500189b2cc42250c3c153aa052d
shubham raj <shubhamraj97@gmail.com>
2016-10-06 20:45:56 +0530

    Update app.js

--- app.js
+++ app.js
@@ -12,13 +12,19 @@ http.listen (app.get('port'),function() {
 });
 io.sockets.on('connection', function (socket) {
   socket.on('join', function (data) {
-      id=data;
+     id=data;
     socket.join(id); // We are using room of socket io
       io.sockets.in(id).emit('got email',id);
     });
-    io.sockets.on('send data',function(data){
+    socket.on('send data',function(data){
+      socket.item=data;
       Itemdata.push(data);
+      console.log(Itemdata);
       io.sockets.in(id).emit('new data',Itemdata);
+      });
+      socket.on('delete',function(data){
+        Itemdata.splice(Itemdata.indexOf(data),1);
+        io.sockets.in(id).emit('deleted',Itemdata);
       });
 
   });
