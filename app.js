6ba102b078fea3cad9464f48f54b0cdba160b518
shubham raj <shubhamraj97@gmail.com>
2016-10-12 16:23:45 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 var express = require('express');
 var app=express();
+var mongoose=require('mongoose');
+
+// connect to the database
+mongoose.connect('mongodb://test:test@ds053186.mlab.com:53186/todolist-shubhamraj');
+var todoSchema = new mongoose.Schema({
+  id:String,
+  list:[String]
+});
+var Todo = mongoose.model('Todo',todoSchema);
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 app.set('port',(process.env.PORT||3000));
@@ -12,10 +21,16 @@ http.listen (app.get('port'),function() {
 });
 io.sockets.on('connection', function (socket) {
   socket.on('join', function (data) {
-     id=data;
+     id=data;var result;
     socket.join(id); // We are using room of socket io
-    Itemdata[id]=[];
       io.sockets.in(id).emit('got email',id);
+      Todo.find({id:id},function(err,result){
+          if(err)
+          {
+            console.log("some error occurred");
+          }
+          
+      });
     });
     socket.on('send data',function(data){
       Itemdata[id].push(data);
